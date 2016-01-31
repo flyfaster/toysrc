1a38996be09c6ace3ea1f355838be3712d967aad
flyfaster <onega@hotmail.com>
2016-01-30 23:02:04 -0800

    add udev rules, pass bdf string

--- mytoybox/linuxdriver/pciemem/pciemem-client.cpp
+++ mytoybox/linuxdriver/pciemem/pciemem-client.cpp
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <string>
 extern "C" {
 #include "pciemem_driver.h"
 };
@@ -13,12 +16,56 @@ char msgbuf[1024];
 
 int main(int argc, char *argv[])
 {
+    namespace po = boost::program_options;
+    boost::program_options::variables_map vm;
+    po::options_description desc("Options");
+    desc.add_options()
+      ("help", "Print help messages")
+	  ("bdf", po::value<std::string>(),  "specify BDF of PCIe device like 00:5.0")
+      ("driverpath", po::value<std::string>(),  "specify driver path like /dev/myclass_mydevice_symlink")
+	  ;
+
+    try
+    {
+    	po::store(po::command_line_parser(argc, argv).options(desc).allow_unregistered().run(),vm);
+      po::notify(vm);
+      if ( vm.count("help") || vm.count("bdf")==0 )
+      {
+        std::cout << argv[0] << " usage:" << std::endl
+                  << desc << std::endl;
+        return 0;
+      }
+    }
+    catch(po::error& e)
+    {
+      std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+      std::cerr << desc << std::endl;
+      return __LINE__;
+    }
+    sprintf(device_path, "/dev/%s_%s", CLASS_NAME, DEVICE_NAME);
+    if(vm.count("driverpath")) {
+    	std::string data = vm["driverpath"].as<std::string>();
+    	strcpy(device_path, data.c_str());
+    }
 	pcie_config_mem_t myarg;
-	if(argc>1)
-		strcpy(myarg.config_space, argv[1]);
-	else
-		return 0;
-	sprintf(device_path, "/dev/%s_%s", CLASS_NAME, DEVICE_NAME);
+    if(vm.count("bdf")) {
+    	std::string data = vm["bdf"].as<std::string>();
+    	int bus=0, device=0, function=0;
+    	std::cout << data << std::endl;
+    	int cnt = sscanf(data.c_str(), "%x:%x.%x", &bus, &device, &function);
+    	if(cnt<3) {
+    		std::cout << "Invalid bdf " << data<<std::endl;
+    		return __LINE__;
+    	}
+    	int bdf = (bus<<8) | (device<<3)|function;
+    	sprintf(myarg.config_space, "0x%x", bdf);
+    	std::cout << myarg.config_space << std::endl;
+    }
+//	if(argc>1)
+//		strcpy(myarg.config_space, argv[1]);
+//	else
+//		return 0;
+
     int fd;
     fd = open(device_path, O_RDONLY);
     if (fd == -1)
@@ -41,7 +88,7 @@ int main(int argc, char *argv[])
     	if( (i%16)==0) {
     		printf("%03x: ",i);
     	}
-    	printf("%02X ", (unsigned int)myarg.config_space[i]&0xFF);
+    	printf("%02x ", (unsigned int)myarg.config_space[i]&0xFF);
     	if (i%16 == 15)
     		printf("\n");
     }
