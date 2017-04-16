93c301f5013808ff248d7070060a255caf65ebd0
flyfaster <test@www.com>
2017-04-16 12:46:42 -0700

    add cmake file, check compiler optimization

--- mytoybox/cppwork/invalidfree.cpp
+++ mytoybox/cppwork/invalidfree.cpp
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <iostream>
+#include <vector>
 #include <boost/current_function.hpp>
 #include <boost/version.hpp>
 #include <boost/config.hpp>
@@ -34,12 +35,34 @@ public:
 	virtual void test() {cout << BOOST_CURRENT_FUNCTION << endl; }
 };
 
+template<typename ContainerType>
+void check_compiler_optimization(size_t data_count) {
+	ContainerType src(data_count);
+	for(size_t i=0; i<src.size(); i++)
+		src[i] = i*i*i*i*i;
+}
+
+class mycontainer: public std::vector<int> {
+public:
+	mycontainer(size_t sz): std::vector<int>(sz) {}
+	~mycontainer() { std::cout << __func__ << " size() called " << call_count << " times." << endl;}
+	mutable size_t call_count=0;
+	size_t size() {
+		call_count++;
+		return std::vector<int>::size();
+	}
+};
+
 int main() {
 	std::cout << "Compiler: " << BOOST_COMPILER
 		<< ", Platform: " << BOOST_PLATFORM 
 		<< ", Library: " << BOOST_STDLIB
 		<< ", Boost " << BOOST_LIB_VERSION
 		<< ", pid " << getpid() << endl;
+	size_t data_count=1024*1024;
+	check_compiler_optimization<vector<int>>(data_count);
+	check_compiler_optimization<mycontainer>(data_count);
+
 	B* objb = new C;
 	objb->test();
 	delete objb;	// -Wdelete-non-virtual-dtor
@@ -74,10 +97,14 @@ clang++ -o ainvalidfree -Weverything -fsanitize=address -fPIE -pie -fno-omit-fra
 
 clang++ -o cinvalidfree -Weverything -fPIE -pie -fno-omit-frame-pointer -g -O2 invalidfree.cpp
 
+g++ -o a2.out -g -O2 invalidfree.cpp && g++ -o a1.out -g -O1 invalidfree.cpp && g++ -o a0.out -g -O0 invalidfree.cpp
+
 g++ 5.4.0 does not report error
 valgrind memcheck or massif does not report error
 valgrind --tool=massif ./cinvalidfree
 cppcheck-1.78 does not report error
 gdb does not report error
+
+eclipse -data ~/workspace/cdt --launcher.openFile invalidfree.cpp
 
 */
