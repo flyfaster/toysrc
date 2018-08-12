fece4e5caafe0bbeb489b86b9aa6e945ec238331
flyfaster <flyfaster@github.com>
2018-08-12 10:36:19 -0700

    subset

--- mytoybox/permutation/permutation.cpp
+++ mytoybox/permutation/permutation.cpp
@@ -104,9 +104,9 @@ struct hash_vec
 };
 
 template <typename PermutationFunc>
-void check_permutation_algorithm(const char* alg_name, PermutationFunc alg)
+void check_complete_search_algorithm(const char* alg_name, PermutationFunc alg)
 {
-    vector<int> nums(10);
+    vector<int> nums(9);
     iota(nums.begin(), nums.end(), 1);
 
     vector<vector<int>> res;
@@ -123,15 +123,46 @@ void check_permutation_algorithm(const char* alg_name, PermutationFunc alg)
          << " milliseconds, there are " << sv.size() << " unique permutations\n";
 }
 
+void gen_subset(vector<int>& nums, int n, vector<vector<int>>& res) {
+	vector<int> subset;
+	std::function<void(int)> search;
+	search = [&nums, &subset, &res, &search](int k) {
+		if (k==nums.size())
+		{
+			res.emplace_back(subset);
+			return;
+		}
+		search(k+1);
+		subset.push_back(nums[k]);
+		search(k+1);
+		subset.pop_back();
+	};
+	search(0);
+}
+
+void gen_subset2(vector<int>& nums, int n, vector<vector<int>>& res) {
+	for (int i=0; i< (1<<n); ++i) {
+		bitset<numeric_limits<int>::digits> bits(i);
+		vector<int> subset;
+		for (int bit=0; bit<n; ++bit) {
+			if (bits[bit])
+				subset.push_back(nums[bit]);
+		}
+		res.emplace_back(move(subset));
+	}
+}
+
 int main()
 {
     std::chrono::system_clock::time_point app_start_time =
         std::chrono::system_clock::now();
-    check_permutation_algorithm("Remove method", permutation1);
-    check_permutation_algorithm("Insert method", permutation2);
-    check_permutation_algorithm("heaps_permutation", heaps_permutation);
-    check_permutation_algorithm("search_permutation", search_permutation);
-    check_permutation_algorithm("stl_permutation", stl_permutation);
+    check_complete_search_algorithm("Remove method", permutation1);
+    check_complete_search_algorithm("Insert method", permutation2);
+    check_complete_search_algorithm("heaps_permutation", heaps_permutation);
+    check_complete_search_algorithm("search_permutation", search_permutation);
+    check_complete_search_algorithm("stl_permutation", stl_permutation);
+    check_complete_search_algorithm("gen_subset", gen_subset);
+    check_complete_search_algorithm("gen_subset2", gen_subset2);
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
         std::chrono::system_clock::now() - app_start_time);
     cout << "Program done in " << duration.count() << " milliseconds\n";
