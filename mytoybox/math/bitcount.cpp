3d633aec5df84f05c93a6f9c6f890a3ae294636b
flyfaster <flyfaster@github.com>
2018-08-19 13:19:01 -0700

    use boost.test

--- mytoybox/math/bitcount.cpp
+++ mytoybox/math/bitcount.cpp
@@ -3,40 +3,72 @@
 #include <limits>
 using namespace std;
 
-int get_bits(int data) {
-	bitset<numeric_limits<decltype(data)>::digits> d_bits(data);
-	return d_bits.count();
+int get_bits(int data)
+{
+    bitset<numeric_limits<decltype(data)>::digits> d_bits(data);
+    return d_bits.count();
 }
 
-int countbits1(int n) {
-	int res = 0;
-	while (n) {
-		++res;
-		n = n & (n - 1); //change the right most bit to 0
-	}
-	return res;
+int countbits1(int n)
+{
+    int res = 0;
+    while (n)
+    {
+        ++res;
+        n = n & (n - 1); // change the right most bit to 0
+    }
+    return res;
 }
 
-int countbits(int n) {
-	int res = 0;
-	for (; n; ++res, n = n & (n - 1))
-		;
-	return res;
+int countbits(int n)
+{
+    int res = 0;
+    for (; n; ++res, n = n & (n - 1))
+        ;
+    return res;
 }
 
-int main() {
-	int mismatch_cnt = 0;
-	for (int i = 0; i < 64; ++i) {
-		auto a = get_bits(i);
-		auto b = countbits1(i);
-		auto c = countbits(i);
-		if (a != b || a != c) {
-			cout << i << " mismatch by counting bits in different ways:" << a
-					<< ", " << b << ", " << c << "\n";
-			++mismatch_cnt;
-		}
-	}
-	if (!mismatch_cnt)
-		cout << "Got the same result by counting bits in different ways.\n";
-	return 0;
+void process_subset(int x)
+{
+    cout << __func__ << "(" << x << ")\n";
+    int b = 0;
+    do
+    {
+        cout << b << " ";
+    } while (b = (b - x) & x);
+    cout << "\n";
+}
+
+int main()
+{
+    int mismatch_cnt = 0;
+    for (int i = 0; i < 64; ++i)
+    {
+        auto a = get_bits(i);
+        auto b = countbits1(i);
+        auto c = countbits(i);
+        if (a != b || a != c)
+        {
+            cout << i << " mismatch by counting bits in different ways:" << a << ", " << b
+                 << ", " << c << "\n";
+            ++mismatch_cnt;
+        }
+        {
+            int x = i;
+            int a = x & ~(x - 1); // only right most bit left, other bits are 0
+            int b = x & (-x);
+            if (a != b || get_bits(a) > 1)
+            {
+                cout << i << " lowest bit " << a << "!=" << b << "\n";
+                ++mismatch_cnt;
+            }
+        }
+    }
+    if (!mismatch_cnt)
+        cout << "Got the same result by counting bits in different ways.\n";
+
+    for (int x = 0; x < 16; ++x)
+        process_subset(x);
+
+    return 0;
 }
---
+++ mytoybox/math/combinatorics.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <utility>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE combinatorics
+//#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+#include <boost/test/results_reporter.hpp>
+
+using namespace std;
+
+int integer_sum_ways(int n)
+{
+    if (n == 0)
+        return 1;
+    int res = 0;
+    for (int i = 0; i < n; i++)
+        res += integer_sum_ways(i);
+    return res;
+}
+
+int integer_sum_ways2(int n)
+{
+    vector<int> cache(n + 1, 0);
+    cache[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; ++j)
+            cache[i] += cache[j];
+    }
+    return cache[n];
+}
+
+BOOST_AUTO_TEST_CASE(check_integer_sum_ways)
+{
+    cout << "BOOST_AUTO_TEST_CASE(counting the number of ways to represent an integer n "
+            "as a sum of positive integers.)"
+         << endl;
+    int n = 9;
+
+    for (int i = 1; i < n; ++i)
+    {
+        int expected = 1 << (i - 1);
+        auto res = integer_sum_ways(i);
+        BOOST_TEST_CONTEXT("integer_sum_ways(" << i << ")")
+        BOOST_CHECK_EQUAL(res, expected);
+    }
+
+    for (int i = 1; i < n; ++i)
+    {
+        int expected = 1 << (i - 1);
+        BOOST_TEST_CONTEXT("integer_sum_ways2(" << i << ")")
+        BOOST_CHECK_EQUAL(integer_sum_ways2(i), expected);
+    }
+}
