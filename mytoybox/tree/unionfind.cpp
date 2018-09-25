8594a56aab6b92297cb830a2470a7dbabe78ae64
flyfaster <flyfaster@github.com>
2018-09-24 22:51:27 -0700

    unionfind

--- mytoybox/tree/unionfind.cpp
+++ mytoybox/tree/unionfind.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <numeric>
 using namespace std;
 
 /*
@@ -24,6 +25,55 @@ link(x, y)
 		if x.rank = y.rank
 			y.rank++
 */
+
+class UnionFind
+{
+    vector<int> p, rank, setSize;
+    int numSets;
+
+public:
+    UnionFind(int N) : p(N, 0), rank(N, 0), setSize(N, 1), numSets(N)
+    {
+        std::iota(p.begin(), p.end(), 0);
+    }
+    int findSet(int i)
+    {
+        return (p[i] == i) ? i : (p[i] = findSet(p[i]));
+    }
+    bool isSameSet(int i, int j)
+    {
+        return findSet(i) == findSet(j);
+    }
+    void unionSet(int i, int j)
+    {
+        if (!isSameSet(i, j))
+        {
+            numSets--;
+            int x = findSet(i), y = findSet(j);
+            // rank is used to keep the tree short
+            if (rank[x] > rank[y])
+            {
+                p[y] = x;
+                setSize[x] += setSize[y];
+            }
+            else
+            {
+                p[x] = y;
+                setSize[y] += setSize[x];
+                if (rank[x] == rank[y])
+                    rank[y]++;
+            }
+        }
+    }
+    int numDisjointSets()
+    {
+        return numSets;
+    }
+    int sizeOfSet(int i)
+    {
+        return setSize[findSet(i)];
+    }
+};
 
 class Solution
 {
